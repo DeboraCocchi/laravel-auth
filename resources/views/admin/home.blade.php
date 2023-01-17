d78bf44b9b76a000c72dc8ee54f248691489ef87
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-17 17:38:47 +0100

    [edit]little-layout-modifications

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,19 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10 mt-5">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">La tua area personale</div>
 
                 <div class="card-body">
-                    @if (session('status'))
+                    {{-- @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!') }} --}}
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-4 p-4">
+                                <img src="{{url('/img/Funny Bunny-7.png')}}" alt="">
+                            </div>
+                            <div class="col-8 p-4">
+
+                                    <p><strong>Nome: </strong>{{ Auth::user()->name }}</p>
+                                <p><strong>Email: </strong>{{ Auth::user()->email }}</p>
+
+
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
